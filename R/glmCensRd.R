878ada197cb9502ad05a114324c2073873cdb653
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2022-03-03 12:05:33 -0500

    Change default steptol to 1E-4

--- R/glmCensRd.R
+++ R/glmCensRd.R
@@ -10,7 +10,7 @@
 #' @param data A dataframe containing at least columns \code{Y}, \code{X}, \code{C}, \code{Z}.
 #' @param distY Distribution assumed for \code{Y} given \code{X} and \code{Z}. Default is \code{"normal"}.
 #' @param distX Distribution assumed for \code{X} given \code{Z}. Default is \code{"normal"}.
-#' @param steptol (Fed to \code{nlm()}) A positive scalar providing the minimum allowable relative step length. Default is \code{1e-6}.
+#' @param steptol (Fed to \code{nlm()}) A positive scalar providing the minimum allowable relative step length. Default is \code{1e-4}.
 #' @param iterlim (Fed to \code{nlm()}) A positive integer specifying the maximum number of iterations to be performed before the program is terminated. Default is \code{100}.
 #'
 #' @return A list with the following elements:
@@ -20,7 +20,7 @@
 #'
 #' @export
 #'
-glmCensRd <- function(params0, Y, X, W, D, Z = NULL, partX = 50, distY = "normal", distX = "normal", data, steptol = 1e-6, iterlim = 100) {
+glmCensRd <- function(params0, Y, X, W, D, Z = NULL, partX = 50, distY = "normal", distX = "normal", data, steptol = 1e-4, iterlim = 100) {
   suppressWarnings(
     mod <- nlm(f = loglik, p = params0, steptol = steptol, iterlim = iterlim, hessian = TRUE,
                Y = Y, X = X, D = D, W = W, Z = Z, partX = partX, distY = distY, distX = distX, data = data)
