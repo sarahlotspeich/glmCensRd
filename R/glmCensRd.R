46998fd423c6e735e5c0790624dd9545dace2efd
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2022-03-07 15:40:02 -0500

    Add initial values for distX = "weibull"

--- R/glmCensRd.R
+++ R/glmCensRd.R
@@ -34,7 +34,7 @@ glmCensRd <- function(Y, W, D, Z = NULL, partX = 50, distY = "normal", distX = "
 
   if (distX %in% c("normal", "log-normal")) {
     params0 <- c(params0, rep(0, length(c(1, Z))), var(data[, X], na.rm = TRUE))
-  } else if (distX %in% c('gamma', "inverse-gaussian")) {
+  } else if (distX %in% c('gamma', "inverse-gaussian", "weibull")) {
     params0 <- c(params0, 0.1, rep(0.1, length(c(1, Z))))
   }
 
