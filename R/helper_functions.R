264e8834e87386ac863bada2b87de58282612c76
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2022-03-10 15:36:35 -0500

    Finish functions second_deriv_pYgivXandZ() and second_deriv_pXgivZ()
    Take second derivatives of each model with respect to its own parameters. Still need a function to take second derivatives across models.

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -204,6 +204,9 @@ part_deriv_pYgivXandZ <- function(y, x, z = NULL, distY, beta_params) {
     }
     all_d[, ncol(all_d)] <- (- (sigY ^ 2) + eY ^ 2) / (2 * (sigY ^ 2) ^ 2) * pYgivXZ # d/dsigma2
     # ------------------ Calculate partial derivatives
+    # Give identifiable column names -----------------
+    colnames(all_d) <- c(paste0("d_beta", 0:(length(beta_params) - 2)), "d_sigmaY2")
+    # ----------------- Give identifiable column names
   } else if (distY == "binomial") {
     # Get parameters ---------------------------------
     ## Construct mean --------------------------------
@@ -220,6 +223,7 @@ part_deriv_pYgivXandZ <- function(y, x, z = NULL, distY, beta_params) {
     # Calculate --------------------------------------
     pYgivXZ <- exp(- (1 - y) * meanY) / (1 + exp(meanY))
     # -------------------------------------- Calculate
+
   }
   return(all_d)
 }
@@ -261,6 +265,9 @@ part_deriv_pXgivZ <- function(x, z = NULL, distX, eta_params) {
     }
     all_d[, ncol(all_d)] <- (- (sigX ^ 2) + eX ^ 2) / (2 * (sigX ^ 2) ^ 2) * pXgivZ # d/dsigma2
     # ------------------ Calculate partial derivatives
+    # Give identifiable column names -----------------
+    colnames(all_d) <- c(paste0("d_eta", 0:(length(eta_params) - 2)), "d_sigmaX2")
+    # ----------------- Give identifiable column names
   }
   return(all_d)
 }
@@ -404,4 +411,134 @@ sandwich_B <- function(params, Y, W, D, Z = NULL, partX = 50, distY = "normal", 
     }
   }
   return(B)
+}
+
+second_deriv_pYgivXandZ <- function(y, x, z = NULL, distY, beta_params) {
+  if (distY == "normal") {
+    # Get parameters ---------------------------------
+    ## Construct mean --------------------------------
+    meanY <- beta_params[1] + beta_params[2] * matrix(data = x, ncol = 1)
+    if (!is.null(z)) {
+      beta2 <- beta_params[-c(1:2, length(beta_params))]
+      if (length(beta2) == 1) {
+        meanY <- meanY + beta2 * z
+      } else {
+        meanY <- meanY + as.numeric(data.matrix(z) %*% matrix(data = beta2, ncol = 1))
+      }
+    }
+    ## Estimate sqrt(variance) directly --------------
+    sigY <- beta_params[length(beta_params)]
+    # --------------------------------- Get parameters
+    # Calculate P(Y|X,Z) -----------------------------
+    eY <- as.numeric(y) - meanY
+    pYgivXZ <- 1 / sqrt(2 * pi * sigY ^ 2) * exp(- eY ^ 2 / (2 * sigY ^ 2))
+    # ----------------------------- Calculate P(Y|X,Z)
+    # Calculate partial derivatives ------------------
+    d_pYgivXZ <- part_deriv_pYgivXandZ(y = y, x = x, z = z, distY = distY, beta_params = beta_params)
+    # ------------------ Calculate partial derivatives
+    # Calculate second derivatives -------------------
+    num_deriv <- sum(length(beta_params) - 0:(length(beta_params) - 1))
+    all_d2 <- matrix(data = NA, nrow = nrow(pYgivXZ), ncol = num_deriv)
+    ## With respect to beta0 -------------------------
+    start_col <- 1
+    end_col <- 3 + ncol(data.frame(z))
+    all_d2[, 1] <- (eY ^ 2 - sigY ^ 2) / ((sigY ^ 2) ^ 2) * pYgivXZ # d2/d2beta02
+    all_d2[, 2] <- x * all_d2[, 1] # d2/dbeta0dbeta1
+    # d/dbeta0dbeta2, ..., d/dbeta0dbetap
+    if (!is.null(z)) {
+      for (c in 1:ncol(data.frame(z))) {
+        all_d2[, (2 + c)] <- data.frame(z)[, c] * all_d2[, 1]
+      }
+    }
+    all_d2[, end_col] <- (- 2 - sigY ^ 2 + eY ^ 2) / (2 * sigY ^ 2) * d_pYgivXZ[, "d_beta0"] # d2/dbeta0dsigma2
+    ## With respect to beta1 --------------------------
+    start_col <- end_col + 1
+    end_col <- end_col + (end_col - 1)
+    all_d2[, start_col] <- x ^ 2 * all_d2[, 1] # d2/d2beta12
+    # d/dbeta1dbeta2, ..., d/dbeta1dbetap
+    if (!is.null(z)) {
+      for (c in 1:ncol(data.frame(z))) {
+        all_d2[, (start_col + c)] <- x * data.frame(z)[, c] * all_d2[, 1]
+      }
+    }
+    all_d2[, end_col] <- (- 2 - sigY ^ 2 + eY ^ 2) / (2 * sigY ^ 2) * d_pYgivXZ[, "d_beta1"] # d2/dbeta1dsigma2
+    ## With respect to beta21, ..., beta2p -----------
+    if (!is.null(z)) {
+      p <- ncol(data.frame(z))
+      for (j in 1:p) {
+        diff <- end_col - start_col
+        start_col <- end_col + 1
+        end_col <- end_col + diff
+        all_d2[, start_col] <- data.frame(z)[, j] ^ 2 * all_d2[, 1] # d2/d2beta2j2
+        if (p > 1) {
+          for (c in 1:(p - j)) {
+            all_d2[, (start_col + c)] <- data.frame(z)[, j] * data.frame(z)[, (c + 1)] * all_d2[, 1]
+          }
+        }
+        all_d2[, end_col] <- (- 2 - sigY ^ 2 + eY ^ 2) / (2 * sigY ^ 2) * d_pYgivXZ[, (2 + j)] # d2/dbeta1dsigma2
+      }
+    }
+    all_d2[, ncol(all_d2)] <- pYgivXZ / ((sigY ^ 2) ^ 2) * (((- sigY ^ 2 + eY ^ 2) / (2 * sigY ^ 2)) + 1) # d2/d2(sigma2)2
+    # ------------------ Calculate partial derivatives
+  }
+  return(all_d2)
+}
+
+second_deriv_pXgivZ <- function(x, z = NULL, distX, eta_params) {
+  if (distX == "normal") {
+    # Get parameters ---------------------------------
+    ## Construct mean --------------------------------
+    meanX <- eta_params[1]
+    if (!is.null(z)) {
+      eta1 <- eta_params[-c(1, length(eta_params))]
+      if (length(eta1) == 1) {
+        meanX <- meanX + eta1 * z
+      } else {
+        meanX <- meanX + as.numeric(data.matrix(z) %*% matrix(data = eta1, ncol = 1))
+      }
+    }
+    ## Estimate sqrt(variance) directly --------------
+    sigX <- eta_params[length(eta_params)]
+    # --------------------------------- Get parameters
+    # Calculate P(Y|X,Z) -----------------------------
+    eX <- data.matrix(x - meanX)
+    pXgivZ <- 1 / sqrt(2 * pi * sigX ^ 2) * exp(- eX ^ 2 / (2 * sigX ^ 2))
+    # ----------------------------- Calculate P(Y|X,Z)
+    # Calculate partial derivatives ------------------
+    d_pXgivZ <- part_deriv_pXgivZ(x = x, z = z, distX = distX, eta_params = eta_params)
+    # ------------------ Calculate partial derivatives
+    # Calculate second derivatives -------------------
+    num_deriv <- sum(length(eta_params) - 0:(length(eta_params) - 1))
+    all_d2 <- matrix(data = NA, nrow = nrow(pXgivZ), ncol = num_deriv)
+    ## With respect to eta0 --------------------------
+    start_col <- 1
+    end_col <- 2 + ncol(data.frame(z))
+    all_d2[, 1] <- (eX ^ 2 - sigX ^ 2) / ((sigX ^ 2) ^ 2) * pXgivZ # d2/d2eta02
+    # d/dbeta0deta1, ..., d/dbeta0dbetap
+    if (!is.null(z)) {
+      for (c in 1:ncol(data.frame(z))) {
+        all_d2[, (1 + c)] <- data.frame(z)[, c] * all_d2[, 1]
+      }
+    }
+    all_d2[, end_col] <- (- 2 - sigX ^ 2 + eX ^ 2) / (2 * sigX ^ 2) * d_pXgivZ[, 1] # d2/dbeta0dsigma2
+    ## With respect to eta1, ..., etap ---------------
+    if (!is.null(z)) {
+      p <- ncol(data.frame(z))
+      for (j in 1:p) {
+        diff <- end_col - start_col
+        start_col <- end_col + 1
+        end_col <- end_col + diff
+        all_d2[, start_col] <- data.frame(z)[, j] ^ 2 * all_d2[, 1] # d2/d2beta2j2
+        if (p > 1) {
+          for (c in 1:(p - j)) {
+            all_d2[, (start_col + c)] <- data.frame(z)[, j] * data.frame(z)[, (c + 1)] * all_d2[, 1]
+          }
+        }
+        all_d2[, end_col] <- (- 2 - sigX ^ 2 + eX ^ 2) / (2 * sigX ^ 2) * d_pYgivXZ[, (1 + j)] # d2/detajdsigma2
+      }
+    }
+    all_d2[, ncol(all_d2)] <- pXgivZ / ((sigX ^ 2) ^ 2) * (((- sigX ^ 2 + eX ^ 2) / (2 * sigX ^ 2)) + 1) # d2/d2(sigma2)2
+    # ------------------ Calculate partial derivatives
+  }
+  return(all_d2)
 }
