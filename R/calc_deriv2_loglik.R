c4e5bcdc0b717f76ab13be882abe4e73660199b4
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2022-03-27 10:10:10 -0500

    Create separate functions for the sandwich estimator

---
+++ R/calc_deriv2_loglik.R
@@ -0,0 +1,42 @@
+#' Calculate the second derivatives of the individual observations' log-likelihood contributions
+#'
+#' @param params Parameter values.
+#' @param Y Name of outcome variable.
+#' @param X Name of censored predictor variable.
+#' @param W Name of observed (i.e., censored) version of \code{X}.
+#' @param D Name of event indicator, defined to be = 1 if \code{X} was uncensored.
+#' @param Z (Optional) name(s) of additional fully observed covariates. Default is \code{NULL}
+#' @param partX Size of partition of unobserved \code{X} for censored subjects. Default is \code{50}.
+#' @param distY Distribution assumed for \code{Y} given \code{X} and \code{Z}.
+#' @param distX Distribution assumed for \code{X} given \code{Z}.
+#' @param data A dataframe containing at least columns \code{Y}, \code{X}, \code{W}, \code{D}, and \code{Z} (if applicable).
+#' @export
+#' @return A vector containing the derivatives of the log-likelihood contributions of the rows/observations in \code{data}.
+#'
+calc_deriv2_loglik <- function(params, Y, X, W, D, Z = NULL, partX = 50, distY = "normal", distX = "normal", data) {
+  p <- length(params)
+  eps <- params * (10 ^ (- 4))
+
+  # Create matrix to save derivatives in
+  d_theta <- matrix(data = 0, nrow = nrow(data), ncol = p ^ 2, byrow = FALSE)
+  start_col <- 1
+  for (j in 1:p) {
+    # Create jth euclidean vector
+    ej <- matrix(data = 0, nrow = p, ncol = 1)
+    ej[j] <- 1
+
+    # params - eps
+    params0 <- matrix(data = params - eps * ej, nrow = p, ncol = 1)
+    d_l0 <- calc_deriv_loglik(params = params0, Y = Y, X = X, W = W, D = D, Z = Z, partX = partX, distY = distX, distX = distX, data = data)
+
+    # params + eps
+    params1 <- matrix(data = params + eps * ej, nrow = p, ncol = 1)
+    d_l1 <- calc_deriv_loglik(params = params1, Y = Y, X = X, W = W, D = D, Z = Z, partX = partX, distY = distX, distX = distX, data = data)
+
+    # Estimate deriv = (l'(beta + eps) - l'(beta - eps)) / (2 * eps)
+    d_theta[, start_col:(start_col + (p - 1))] <- (d_l1 - d_l0) / (2 * eps[j])
+    start_col <- start_col + p
+  }
+
+  return(d_theta)
+}
---
+++ R/calc_indiv_loglik.R
@@ -0,0 +1,143 @@
+#' Calculate individual observations' log-likelihood contributions 
+#'
+#' @param params Parameter values.
+#' @param Y Name of outcome variable.
+#' @param X Name of censored predictor variable.
+#' @param W Name of observed (i.e., censored) version of \code{X}.
+#' @param D Name of event indicator, defined to be = 1 if \code{X} was uncensored.
+#' @param Z (Optional) name(s) of additional fully observed covariates. Default is \code{NULL}
+#' @param partX Size of partition of unobserved \code{X} for censored subjects. Default is \code{50}.
+#' @param distY Distribution assumed for \code{Y} given \code{X} and \code{Z}.
+#' @param distX Distribution assumed for \code{X} given \code{Z}.
+#' @param data A dataframe containing at least columns \code{Y}, \code{X}, \code{W}, \code{D}, and \code{Z} (if applicable).
+#' @export
+#' @return A vector containing the log-likelihood contributions of the rows/observations in \code{data}.
+#'
+calc_indiv_loglik <- function(params, Y, X, W, D, Z = NULL, partX = 50, distY, distX, data) {
+  ####################################################
+  # Pre-processing ###################################
+  ####################################################
+  # < number of uncensored subjects > ----------------
+  n1 <- sum(data[, D]) # -----------------------------
+  # ---------------- < number of uncensored subjects >
+  # Reordered data to be uncensored first ------------
+  data <- data[order(data[, D], decreasing = TRUE), ]
+  # ------------ Reordered data to be uncensored first
+  # Create subset of uncensored subjects' data -------
+  uncens_data <- data[1:n1, ]
+  # ------- Create subset of uncensored subjects' data
+  # Create subset of censored subjects' data -------
+  cens_data <- data[-c(1:n1), ]
+  # ------- Create subset of censored subjects' data
+  ####################################################
+  # Analysis model P(Y|X,Z) ##########################
+  ####################################################
+  if (distY == "normal") {
+    # Subset parameters ------------------------------
+    beta_params <- params[1:(3 + length(Z))]
+    # ------------------------------ Subset parameters
+  } else if (distY == "binomial") {
+    # Subset parameters ------------------------------
+    beta_params <- params[1:(2 + length(Z))]
+    # ------------------------------ Subset parameters
+  }
+  pYgivXZ <- calc_pYgivXandZ(y = uncens_data[, Y], x = uncens_data[, X], z = uncens_data[, Z], distY = distY, beta_params = beta_params)
+  
+  ####################################################
+  # Predictor model P(X|Z) ###########################
+  ####################################################
+  # Subset parameters --------------------------------
+  eta_params <- params[-c(1:length(beta_params))]
+  # -------------------------------- Subset parameters
+  # Check for parameters outside domain --------------
+  if (distX == "gamma") {
+    # Shape and scale of Gamma > 0 -------------------
+    shapeX <- eta_params[1]
+    meanX <- eta_params[2]
+    if (length(Z) > 0) {
+      meanX <- meanX + as.numeric(data.matrix(uncens_data[, Z]) %*% matrix(data = eta_params[3:(2 + length(Z))], ncol = 1))
+    }
+    scaleX <- meanX / shapeX
+    if (any(c(shapeX, scaleX) <= 0)) { return(99999)}
+    # ------------------- Shape and scale of Gamma > 0
+  } else if (distX == "inverse-gaussian") {
+    # Shape and mean of inverse-Gaussian both > 0 ----
+    shapeX <- eta_params[1]
+    meanX <- eta_params[2]
+    if (length(Z) > 0) {
+      meanX <- meanX + as.numeric(data.matrix(uncens_data[, Z]) %*% matrix(data = eta_params[3:(2 + length(Z))], ncol = 1))
+    }
+    if (any(c(shapeX, meanX) <= 0)) { return(99999)}
+    # --------------------------------- Get parameters
+  } else if (distX == "weibull") {
+    # Shape and scale of Weibull both > 0 ------------
+    shapeX <- eta_params[1]
+    scaleX <- eta_params[2]
+    if (length(Z) > 0) {
+      eta1 <- eta_params[3:(2 + length(Z))]
+      scaleX <- scaleX + as.numeric(data.matrix(uncens_data[, Z]) %*% matrix(data = eta1, ncol = 1))
+    }
+    if (any(c(shapeX, scaleX) <= 0)) { return(99999)}
+  } else if (distX %in% c("exponential", "poisson")) {
+    # Rate of Exponential or Poisson > 0 -------------
+    rateX <- eta_params[1]
+    if (length(Z) > 0) {
+      eta1 <- eta_params[2:(1 + length(Z))]
+      if (length(eta1) == 1) {
+        rateX <- rateX + eta1 * uncens_data[, Z]
+      } else {
+        rateX <- rateX + as.numeric(data.matrix(uncens_data[, Z]) %*% matrix(data = eta1, ncol = 1))
+      }
+    }
+    if (any(rateX <= 0)) { return (99999) }
+  }
+  pXgivZ <- calc_pXgivZ(x = uncens_data[, X], z = uncens_data[, Z], distX = distX, eta_params = eta_params)
+  
+  ####################################################
+  # Calculate joint density P(Y,X,Z) #################
+  ####################################################
+  uncens_data <- cbind(uncens_data, jointP = 1)
+  uncens_data[, "jointP"] <- pYgivXZ * pXgivZ
+  #uncens_data <- data.frame(cbind(uncens_data, jointP = pYgivXZ * pXgivZ))
+  
+  ####################################################
+  # Calculate the log-likelihood #####################
+  ####################################################
+  # Log-likelihood contribution of uncensored X ------
+  ll <- log(uncens_data[, "jointP"])
+  # ------ Log-likelihood contribution of uncensored X
+  # Log-likelihood contribution of censored X --------
+  joint_dens <- function(x, Yi, Zi) {
+    ####################################################
+    # Analysis model P(Y|X,Z) ##########################
+    ####################################################
+    pYgivXZ <- calc_pYgivXandZ(y = Yi, x = x, z = Zi, distY = distY, beta_params = beta_params)
+    
+    ####################################################
+    # Predictor model P(X|Z) ###########################
+    ####################################################
+    pXgivZ <- calc_pXgivZ(x = x, z = Zi, distX = distX, eta_params = eta_params)
+    
+    ####################################################
+    # Joint density P(Y,X,Z) ###########################
+    ####################################################
+    return(pYgivXZ * pXgivZ)
+  }
+  integrate_joint_dens <- function(data_row) {
+    data_row <- data.frame(t(data_row))
+    return(
+      tryCatch(expr = integrate(f = joint_dens, lower = data_row[, W], upper = Inf, subdivisions = partX,
+                                Yi = data_row[Y], Zi = data_row[, Z])$value,
+               error = function(err) {0})
+    )
+  }
+  integral <- apply(X = cens_data, MARGIN = 1, FUN = integrate_joint_dens)
+  log_integral <- log(integral)
+  log_integral[log_integral == -Inf] <- 0
+  ll <- append(ll, log_integral)
+  # -------- Log-likelihood contribution of censored X
+  # Return (-1) x log-likelihood for use with nlm() --
+  ll[ll == - Inf] <- 0
+  return(ll)
+  # -- Return (-1) x log-likelihood for use with nlm()
+}
