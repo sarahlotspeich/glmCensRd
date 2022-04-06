1df52790ac66846df528a7080a5de88719b2a632
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2022-04-06 17:38:02 -0400

    Add initial_values to documentation and output, give complete-case naive start

--- R/glmCensRd.R
+++ R/glmCensRd.R
@@ -9,13 +9,14 @@
 #' @param distY Distribution assumed for \code{Y} given \code{X} and \code{Z}. Default is \code{"normal"}.
 #' @param distX Distribution assumed for \code{X} given \code{Z}. Default is \code{"normal"}.
 #' @param estSE If \code{TRUE}, robust sandwich estimators of parameter standard errors are included in the output. Default is \code{FALSE}.
-#' @param estSE A string input for which initial values to use in \code{nlm}. Options include \code{"complete-case"} (the default) and \code{"naive"}.
+#' @param initial_values A string input for which initial values to use in \code{nlm}. Options include \code{"complete-case"} (the default) and \code{"naive"}.
 #' @param steptol (Fed to \code{nlm}) A positive scalar providing the minimum allowable relative step length. Default is \code{1E-6}.
 #' @param iterlim (Fed to \code{nlm}) A positive integer specifying the maximum number of iterations to be performed before the program is terminated. Default is \code{100}.
 #'
 #' @return A list with the following elements:
 #' \item{outcome_model}{A list containing details of the fitted model for the outcome.}
 #' \item{predictor_model}{A list containing details of the fitted model for the predictor.}
+#' \item{initial_values}{A string indicating which starting values were used in the algorithm.}
 #' \item{code}{An integer indicating why the optimization process terminated. See \code{?nlm} for details on values.}
 #'
 #' @export
@@ -31,6 +32,24 @@ glmCensRd <- function(Y, W, D, Z = NULL, partX = 50, data,  distY = "normal", di
   X <- "X"
 
   # Initial parameter values
+  ## Naive
+  if (distY == "normal") {
+    params0 <- c(rep(0, length(c(1, X, Z))), sd(data[, Y]))
+  } else if (distY == "binomial") {
+    params0 <- c(rep(0, length(c(1, X, Z))))
+  }
+
+  if (distX %in% c("normal", "log-normal")) {
+    params0 <- c(params0, rep(0, length(c(1, Z))), sd(data[, X], na.rm = TRUE))
+  } else if (distX %in% c('gamma', "inverse-gaussian")) {
+    params0 <- c(params0, 0.1, rep(0.1, length(c(1, Z))))
+  } else if (distX == "weibull") {
+    params0 <- c(params0, 1, rep(0.1, length(c(1, Z))))
+  } else if (distX %in% c("exponential", "poisson")) {
+    params0 <- c(params0, rep(0.1, length(c(1, Z))))
+  }
+
+  ## If complete-case was selected, update but use naive as a start
   if (initial_values == "complete-case") {
     suppressWarnings(
       cc_mod <- nlm(f = loglik,
@@ -51,44 +70,6 @@ glmCensRd <- function(Y, W, D, Z = NULL, partX = 50, data,  distY = "normal", di
     )
     if (cc_mod$code <= 3) {
       params0 <- cc_mod$estimate
-    } else {
-      initial_values <- "naive"
-    }
-    # if (distY == "normal") {
-    #   cc_outcome_model <- glm(formula = as.formula(paste(Y, "~", X, "+", paste(Z, collapse = "+"))),
-    #                           data = data, family = "gaussian")
-    #   params0 <- c(cc_outcome_model$coefficients, sigma(cc_outcome_model))
-    # } else if (distY == "binomial") {
-    #   #params0 <- c(rep(0, length(c(1, X, Z))))
-    # }
-    #
-    # if (distX == "normal") {
-    #   cc_predictor_model <- glm(formula = as.formula(paste(X, "~", paste(Z, collapse = "+"))),
-    #                           data = data, family = "gaussian")
-    #   params0 <- c(params0, cc_predictor_model$coefficients, sigma(cc_predictor_model))
-    #   #params0 <- c(params0, rep(0, length(c(1, Z))), sd(data[, X], na.rm = TRUE))
-    # } else if (distX %in% c('gamma', "inverse-gaussian")) {
-    #   #params0 <- c(params0, 0.1, rep(0.1, length(c(1, Z))))
-    # } else if (distX == "weibull") {
-    #   #params0 <- c(params0, 1, rep(0.1, length(c(1, Z))))
-    # } else if (distX %in% c("exponential", "poisson")) {
-    #   #params0 <- c(params0, rep(0.1, length(c(1, Z))))
-    # }
-  } else if (initial_values == "naive") {
-    if (distY == "normal") {
-      params0 <- c(rep(0, length(c(1, X, Z))), sd(data[, Y]))
-    } else if (distY == "binomial") {
-      params0 <- c(rep(0, length(c(1, X, Z))))
-    }
-
-    if (distX %in% c("normal", "log-normal")) {
-      params0 <- c(params0, rep(0, length(c(1, Z))), sd(data[, X], na.rm = TRUE))
-    } else if (distX %in% c('gamma', "inverse-gaussian")) {
-      params0 <- c(params0, 0.1, rep(0.1, length(c(1, Z))))
-    } else if (distX == "weibull") {
-      params0 <- c(params0, 1, rep(0.1, length(c(1, Z))))
-    } else if (distX %in% c("exponential", "poisson")) {
-      params0 <- c(params0, rep(0.1, length(c(1, Z))))
     }
   }
 
@@ -205,5 +186,5 @@ glmCensRd <- function(Y, W, D, Z = NULL, partX = 50, data,  distY = "normal", di
     modX <- list(distX = distX, rate = modX_coeff)
   }
 
-  return(list(outcome_model = modY, predictor_model = modX, code = mod$code))
+  return(list(outcome_model = modY, predictor_model = modX, initial_values = initial_values, code = mod$code))
 }
