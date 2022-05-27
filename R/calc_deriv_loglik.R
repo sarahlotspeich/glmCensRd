be07b0c1f6f0781e2b3c35ee86174bbeede3112c
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2022-05-27 08:34:57 -0400

    Rename partX --> subdivisions in calc_deriv_loglik()

--- R/calc_deriv_loglik.R
+++ R/calc_deriv_loglik.R
@@ -1,19 +1,19 @@
 #' Calculate the first derivatives of the individual observations' log-likelihood contributions
 #'
-#' @param params Parameter values.
-#' @param Y Name of outcome variable.
-#' @param X Name of censored predictor variable.
-#' @param W Name of observed (i.e., censored) version of \code{X}.
-#' @param D Name of event indicator, defined to be = 1 if \code{X} was uncensored.
-#' @param Z (Optional) name(s) of additional fully observed covariates. Default is \code{NULL}
-#' @param partX Size of partition of unobserved \code{X} for censored subjects. Default is \code{50}.
-#' @param distY Distribution assumed for \code{Y} given \code{X} and \code{Z}.
-#' @param distX Distribution assumed for \code{X} given \code{Z}.
-#' @param data A dataframe containing at least columns \code{Y}, \code{X}, \code{W}, \code{D}, and \code{Z} (if applicable).
+#' @param params parameter values.
+#' @param Y name of outcome variable.
+#' @param X name of censored predictor variable.
+#' @param W name of observed (censored) version of \code{X}.
+#' @param D name of event indicator, defined to be \code{= 1} if \code{X} was uncensored and \code{0} otherwise.
+#' @param Z (optional) name(s) of additional fully observed covariates. Default is \code{NULL}.
+#' @param data a dataframe containing at least columns \code{Y}, \code{X}, \code{C}, \code{Z}.
+#' @param subdivisions (fed to \code{integrate}) the maximum number of subintervals used to integrate over unobserved \code{X} for censored subjects. Default is \code{100}.
+#' @param distY distribution assumed for \code{Y} given \code{X} and \code{Z}. Default is \code{"normal"}, but \code{"binomial"} is the other option.
+#' @param distX distribution assumed for \code{X} given \code{Z}. Default is \code{"normal"}, but other options are \code{"log-normal"}, \code{"gamma"}, \code{"inverse-gaussian"}, \code{"weibull"}, \code{"exponential"}, or \code{"poisson"}.
 #' @export
 #' @return A vector containing the derivatives of the log-likelihood contributions of the rows/observations in \code{data}.
 #'
-calc_deriv_loglik <- function(params, Y, X, W, D, Z = NULL, partX = 50, distY = "normal", distX = "normal", data) {
+calc_deriv_loglik <- function(params, Y, X, W, D, Z = NULL, data, subdivisions = 100, distY = "normal", distX = "normal") {
   p <- length(params)
   eps <- params * (10 ^ (- 4))
 
@@ -26,11 +26,11 @@ calc_deriv_loglik <- function(params, Y, X, W, D, Z = NULL, partX = 50, distY = 
 
     # params - eps
     params0 <- matrix(data = params - eps * ej, nrow = p, ncol = 1)
-    l0 <- calc_indiv_loglik(params = params0, Y = Y, X = X, W = W, D = D, Z = Z, partX = partX, distY = distY, distX = distX, data = data)
+    l0 <- calc_indiv_loglik(params = params0, Y = Y, X = X, W = W, D = D, Z = Z, subdivisions = subdivisions, distY = distY, distX = distX, data = data)
 
     # params + eps
     params1 <- matrix(data = params + eps * ej, nrow = p, ncol = 1)
-    l1 <- calc_indiv_loglik(params = params1, Y = Y, X = X, W = W, D = D, Z = Z, partX = partX, distY = distY, distX = distX, data = data)
+    l1 <- calc_indiv_loglik(params = params1, Y = Y, X = X, W = W, D = D, Z = Z, subdivisions = subdivisions, distY = distY, distX = distX, data = data)
 
     # Estimate deriv = (l(beta + eps) - l(beta - eps)) / (2 * eps)
     d_theta[, j] <- (l1 - l0) / (2 * eps[j])
