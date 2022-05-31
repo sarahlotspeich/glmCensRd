d9be1afbc88330879421158b3e3c0d97bd2fd296
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2022-05-31 18:49:29 -0400

    Remove W argument to cc_loglik()

--- R/cc_loglik.R
+++ R/cc_loglik.R
@@ -3,7 +3,6 @@
 #' @param params parameter values.
 #' @param Y name of outcome variable.
 #' @param X name of censored predictor variable.
-#' @param W name of observed (censored) version of \code{X}.
 #' @param Z (optional) name(s) of additional fully observed covariates. If none, \code{Z = NULL} (the default).
 #' @param data a dataframe containing at least columns \code{Y}, \code{X}, \code{C}, \code{Z}.
 #' @param distY distribution assumed for \code{Y} given \code{X} and \code{Z}. Default is \code{"normal"}, but other options are \code{"binomial"}, \code{"log-normal"}, \code{"weibull"}, \code{"exponential"}, or \code{"poisson"}.
@@ -13,7 +12,7 @@
 #'
 #' @return A scalar of the log-likelihood function (negated for use with \code{nlm}()).
 #'
-cc_loglik <- function(params, Y, X, W, Z = NULL, data, distY = "normal", distX = "normal") {
+cc_loglik <- function(params, Y, X, Z = NULL, data, distY = "normal", distX = "normal") {
   ####################################################
   # Joint density P(Y,X,Z) ###########################
   ####################################################
