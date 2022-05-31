a575acd678c9d09c4a8c9cc508ac7532257ed7ac
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2022-05-31 16:25:18 -0400

    Feed full vector of params (rather than separate beta/eta) into calc_pYXandZ()

--- R/calc_pYXandZ.R
+++ R/calc_pYXandZ.R
@@ -4,16 +4,31 @@
 #' @param x Predictor values (scalar or vector).
 #' @param z Covariate values (scalar, vector, or dataframe).
 #' @param distY Distribution assumed for \code{y} given \code{x} and \code{z}.
-#' @param beta_params Vector of model parameters.
 #' @param distX Distribution assumed for \code{x} given \code{z}.
-#' @param eta_params Vector of model parameters.
+#' @param params parameter values.
 #'
 #' @return A scalar or numeric vector the same length as the data input
 #'
 #' @export
 #'
 #'
-calc_pYXandZ <- function(y, x, z = NULL, distY, beta_params, distX, eta_params) {
+calc_pYXandZ <- function(y, x, z = NULL, distY, distX, params) {
+  ####################################################
+  # Separate params into models ######################
+  ####################################################
+  # Analysis model ///////////////////////////////////
+  if (distY %in% c("normal", "log-normal", "weibull")) {
+    # Subset parameters ------------------------------
+    beta_params <- params[1:(3 + length(Z))]
+    # ------------------------------ Subset parameters
+  } else if (distY %in% c("binomial", "exponential", "poisson")) {
+    # Subset parameters ------------------------------
+    beta_params <- params[1:(2 + length(Z))]
+    # ------------------------------ Subset parameters
+  }
+  # Predictor model //////////////////////////////////
+  eta_params <- params[-c(1:length(beta_params))]
+
   # Calculate P(Y|X,Z)
   pYgivXandZ <- calc_pYgivXandZ(y = y,
                                 x = x,
