20ea1e5b45c449292e35fc5ded9d3d2448e6211d
Peter Guan <33794703+pyiguan@users.noreply.github.com>
2022-05-31 11:58:02 -0400

    Update calc_pYgivXandZ.R

--- R/calc_pYgivXandZ.R
+++ R/calc_pYgivXandZ.R
@@ -48,6 +48,21 @@ calc_pYgivXandZ <- function(y, x, z = NULL, distY, beta_params) {
     # pYgivXZ <- exp(- (1 - y) * meanY) / (1 + exp(meanY))
     # pYgivXZ <- dbinom(x = as.numeric(y), size = 1, prob = 1 / (1 + exp(- meanY)))
     # -------------------------------------- Calculate
+  } else if (distY == "poisson") {
+    # Get parameters -----------------------------------
+    # ----------------------------------- Get parameters
+    # Calculate ----------------------------------------
+    muY <- exp(beta_params[1] + beta_params[2] * matrix(data = x, ncol = 1))
+    if (!is.null(z)) {
+      beta2 <- beta_params[-c(1:2)]
+      if (length(beta2) == 1) {
+        muY <- muY * exp(beta2 * z)
+      } else {
+      muY <- muY * exp(as.numeric(data.matrix(z) %*% matrix(data = beta2, ncol = 1)))
+      }
+    }
+    pYgivXZ <- ((muY)^(as.numeric(y))) * exp(-muY) / factorial(as.numeric(y))
+    # ---------------------------------------- Calculate
   }
   return(pYgivXZ)
 }
