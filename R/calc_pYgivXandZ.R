a2fb5302a15415da22351c6ab52051b4c162ab50
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2022-05-31 13:03:15 -0400

    Replace hard-coded densities in calc_pYgivXZ with base-R functions
    Also added distY = "log-normal", "binomial", "poisson", or "exponential"

--- R/calc_pYgivXandZ.R
+++ R/calc_pYgivXandZ.R
@@ -12,7 +12,7 @@
 #'
 #'
 calc_pYgivXandZ <- function(y, x, z = NULL, distY, beta_params) {
-  if (distY == "normal") {
+  if (distY %in% c("normal", "log-normal")) {
     # Get parameters ---------------------------------
     ## Construct mean --------------------------------
     meanY <- beta_params[1] + beta_params[2] * matrix(data = x, ncol = 1)
@@ -25,11 +25,16 @@ calc_pYgivXandZ <- function(y, x, z = NULL, distY, beta_params) {
       }
     }
     ## Estimate sqrt(variance) directly --------------
-    sigY <- beta_params[length(beta_params)]
+    sigY <- sqrt(beta_params[length(beta_params)] ^ 2)
     # --------------------------------- Get parameters
     # Calculate --------------------------------------
-    eY <- as.numeric(y) - meanY
-    pYgivXZ <- 1 / sqrt(2 * pi * sigY ^ 2) * exp(- eY ^ 2 / (2 * sigY ^ 2))
+    # eY <- as.numeric(y) - meanY
+    if (distY == "normal") {
+      # pYgivXZ <- 1 / sqrt(2 * pi * sigY ^ 2) * exp(- eY ^ 2 / (2 * sigY ^ 2))
+      pYgivXZ <- dnorm(x = y, mean = meanY, sd = sigY)
+    } else {
+      pYgivXZ <- dlnorm(x = y, meanlog = meanY, sdlog = sigY)
+    }
     # -------------------------------------- Calculate
   } else if (distY == "binomial") {
     # Get parameters ---------------------------------
@@ -45,25 +50,31 @@ calc_pYgivXandZ <- function(y, x, z = NULL, distY, beta_params) {
     }
     # --------------------------------- Get parameters
     # Calculate --------------------------------------
-    # pYgivXZ_1 <-
+    pYgivXZ <- dbinom(x = y, size = 1, prob = 1 / (1 + exp(- meanY)))
+    pYgivXZ[y == 0] <- 1 - pYgivXZ[y == 0]
     # pYgivXZ <- exp(- (1 - y) * meanY) / (1 + exp(meanY))
     # pYgivXZ <- dbinom(x = as.numeric(y), size = 1, prob = 1 / (1 + exp(- meanY)))
     # -------------------------------------- Calculate
-  } else if (distY == "poisson") {
-    # Get parameters -----------------------------------
-    # ----------------------------------- Get parameters
-    # Calculate ----------------------------------------
-    muY <- exp(beta_params[1] + beta_params[2] * matrix(data = x, ncol = 1))
+  } else if (distY %in% c("exponential", "poisson")) {
+    # Get parameters ---------------------------------
+    ## Construct rate  -------------------------------
+    rateY <- beta_params[1] + beta_params[2] * x
     if (!is.null(z)) {
       beta2 <- beta_params[-c(1:2)]
       if (length(beta2) == 1) {
-        muY <- muY * exp(beta2 * z)
+        rateY <- rateY + beta2 * z
       } else {
-      muY <- muY * exp(as.numeric(data.matrix(z) %*% matrix(data = beta2, ncol = 1)))
+        rateY <- rateY + as.numeric(data.matrix(z) %*% matrix(data = beta2, ncol = 1))
       }
     }
-    pYgivXZ <- ((muY)^(as.numeric(y))) * exp(-muY) / factorial(as.numeric(y))
-    # ---------------------------------------- Calculate
+    # --------------------------------- Get parameters
+    # Calculate --------------------------------------
+    if (distX == "exponential") {
+      pYgivXZ <- dexp(x = y, rate = rateY)
+    } else {
+      pYgivXZ <- dpois(x = y, lambda = rateY)
+    }
+    # -------------------------------------- Calculate
   }
   return(pYgivXZ)
 }
