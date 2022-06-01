fb659f0e3bb6df38e339666801dfb4b21307d562
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2022-06-01 12:59:23 -0400

    Forgot some distY in init_vals()

--- R/init_vals.R
+++ R/init_vals.R
@@ -5,6 +5,8 @@ init_vals <- function(Z, distY, distX) {
     params0 <- c(rep(0, length(c(1, 1, Z))))
   } else if (distY %in% c('gamma', "inverse-gaussian", "weibull")) {
     params0 <- rep(1E-4, 3 + length(Z))
+  } else if (distY %in% c("exponential", "poisson")) {
+    params0 <- c(params0, rep(1E-4, 1 + length(Z)))
   }
 
   if (distX %in% c("normal", "log-normal")) {
