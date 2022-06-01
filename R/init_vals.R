ee975832318e7eeaea289768f755c462d3e82fe8
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2022-06-01 12:58:11 -0400

    Add initial values for new options of distY

--- R/init_vals.R
+++ R/init_vals.R
@@ -3,16 +3,16 @@ init_vals <- function(Z, distY, distX) {
     params0 <- c(rep(0, length(c(1, 1, Z))), 1)
   } else if (distY == "binomial") {
     params0 <- c(rep(0, length(c(1, 1, Z))))
+  } else if (distY %in% c('gamma', "inverse-gaussian", "weibull")) {
+    params0 <- rep(1E-4, 3 + length(Z))
   }
 
   if (distX %in% c("normal", "log-normal")) {
     params0 <- c(params0, rep(0, length(c(1, Z))), 1)
-  } else if (distX %in% c('gamma', "inverse-gaussian")) {
-    params0 <- c(params0, 1E-4, rep(1E-4, length(c(1, Z))))
-  } else if (distX == "weibull") {
-    params0 <- c(params0, 1E-4, rep(1E-4, length(c(1, Z))))
+  } else if (distX %in% c('gamma', "inverse-gaussian", "weibull")) {
+    params0 <- c(params0, rep(1E-4, 2 + length(Z)))
   } else if (distX %in% c("exponential", "poisson")) {
-    params0 <- c(params0, rep(1E-4, length(c(1, Z))))
+    params0 <- c(params0, rep(1E-4, 1 + length(Z)))
   }
 
   return(params0)
