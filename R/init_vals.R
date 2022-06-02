2a52e14176ff93bc9e8793199cfae2b6d7576ab9
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2022-06-01 20:57:36 -0400

    Replace single quote with double

--- R/init_vals.R
+++ R/init_vals.R
@@ -3,7 +3,7 @@ init_vals <- function(Z, distY, distX) {
     params0 <- c(rep(0, 2 + length(Z)), 1)
   } else if (distY == "binomial") {
     params0 <- c(rep(0, 2 + length(Z)))
-  } else if (distY %in% c('gamma', "inverse-gaussian", "weibull")) {
+  } else if (distY %in% c("gamma", "inverse-gaussian", "weibull")) {
     params0 <- rep(1E-4, 3 + length(Z))
   } else if (distY %in% c("exponential", "poisson")) {
     params0 <- rep(1E-4, 2 + length(Z))
@@ -11,7 +11,7 @@ init_vals <- function(Z, distY, distX) {
 
   if (distX %in% c("normal", "log-normal")) {
     params0 <- c(params0, rep(0, 1 + length(Z)), 1)
-  } else if (distX %in% c('gamma', "inverse-gaussian", "weibull")) {
+  } else if (distX %in% c("gamma", "inverse-gaussian", "weibull")) {
     params0 <- c(params0, rep(1E-4, 2 + length(Z)))
   } else if (distX %in% c("exponential", "poisson")) {
     params0 <- c(params0, rep(1E-4, 1 + length(Z)))
