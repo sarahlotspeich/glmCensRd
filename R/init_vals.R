7501a9c74f756aaf4f5c36f00f4c51775d9fde5f
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2022-06-02 15:31:09 -0400

    Change initial values to be as "naive" as possible

--- R/init_vals.R
+++ R/init_vals.R
@@ -6,15 +6,15 @@ init_vals <- function(Z, distY, distX) {
   } else if (distY %in% c("gamma", "inverse-gaussian", "weibull")) {
     params0 <- rep(1E-4, 3 + length(Z))
   } else if (distY %in% c("exponential", "poisson")) {
-    params0 <- rep(1E-4, 2 + length(Z))
+    params0 <- c(1E-4, rep(0, 1 + length(Z)))
   }
 
   if (distX %in% c("normal", "log-normal")) {
     params0 <- c(params0, rep(0, 1 + length(Z)), 1)
   } else if (distX %in% c("gamma", "inverse-gaussian", "weibull")) {
-    params0 <- c(params0, rep(1E-4, 2 + length(Z)))
+    params0 <- c(params0, 1E-4, rep(0, 1 + length(Z)))
   } else if (distX %in% c("exponential", "poisson")) {
-    params0 <- c(params0, rep(1E-4, 1 + length(Z)))
+    params0 <- c(params0, 1E-4, rep(0, length(Z)))
   }
 
   return(params0)
