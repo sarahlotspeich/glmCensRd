89a833d574f379a213468de2cd4ae23d8a33c54a
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2022-06-01 20:45:07 -0400

    Fix init_vals()

--- R/init_vals.R
+++ R/init_vals.R
@@ -1,8 +1,8 @@
 init_vals <- function(Z, distY, distX) {
   if (distY %in% c("normal", "log-normal")) {
-    params0 <- c(rep(0, length(c(1, 1, Z))), 1)
+    params0 <- c(rep(0, 2 + length(Z)), 1)
   } else if (distY == "binomial") {
-    params0 <- c(rep(0, length(c(1, 1, Z))))
+    params0 <- c(rep(0, 2 + length(Z)))
   } else if (distY %in% c('gamma', "inverse-gaussian", "weibull")) {
     params0 <- rep(1E-4, 3 + length(Z))
   } else if (distY %in% c("exponential", "poisson")) {
@@ -10,7 +10,7 @@ init_vals <- function(Z, distY, distX) {
   }
 
   if (distX %in% c("normal", "log-normal")) {
-    params0 <- c(params0, rep(0, length(c(1, Z))), 1)
+    params0 <- c(params0, rep(0, 1 + length(Z)), 1)
   } else if (distX %in% c('gamma', "inverse-gaussian", "weibull")) {
     params0 <- c(params0, rep(1E-4, 2 + length(Z)))
   } else if (distX %in% c("exponential", "poisson")) {
