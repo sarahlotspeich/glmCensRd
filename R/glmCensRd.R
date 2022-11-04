b41ea92a82d696a8d8fd6aa0586d2db02ef0c799
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2022-11-04 14:47:46 -0400

    Add "cens" param to glmCensRd()

--- R/glmCensRd.R
+++ R/glmCensRd.R
@@ -4,9 +4,10 @@
 #' @param W name of observed (censored) version of \code{X}.
 #' @param D name of event indicator, defined to be \code{= 1} if \code{X} was uncensored and \code{0} otherwise.
 #' @param Z (optional) name(s) of additional fully observed covariates. If none, \code{Z = NULL} (the default).
-#' @param data a dataframe containing at least columns \code{Y}, \code{X}, \code{C}, \code{Z}.
+#' @param data dataframe containing at least columns \code{Y}, \code{X}, \code{C}, \code{Z}.
 #' @param distY distribution assumed for \code{Y} given \code{X} and \code{Z}. Default is \code{"normal"}, but other options are \code{"binomial"}, \code{"log-normal"}, \code{"weibull"}, \code{"exponential"}, or \code{"poisson"}.
 #' @param distX distribution assumed for \code{X} given \code{Z}. Default is \code{"normal"}, but other options are \code{"log-normal"}, \code{"gamma"}, \code{"weibull"}, \code{"exponential"}, or \code{"poisson"}.
+#' @param cens type of censoring assumed for \code{X}. Default is \code{"right"}, but the other option is \code{"left"}.
 #' @param robcov logical. If \code{TRUE} (the default), the robust sandwich covariance estimator of parameter standard errors is included in the output.
 #' @param subdivisions (fed to \code{integrate}) the maximum number of subintervals used to integrate over unobserved \code{X} for censored subjects. Default is \code{100}.
 #' @param steptol (fed to \code{nlm}) a positive scalar providing the minimum allowable relative step length. Default is \code{1E-6}.
@@ -22,7 +23,7 @@
 #'
 #' @export
 #'
-glmCensRd <- function(Y, W, D, Z = NULL, data,  distY = "normal", distX = "normal", robcov = TRUE, subdivisions = 100, steptol = 1E-6, iterlim = 100, verbose = FALSE) {
+glmCensRd <- function(Y, W, D, Z = NULL, data,  distY = "normal", distX = "normal", cens = "right", robcov = TRUE, subdivisions = 100, steptol = 1E-6, iterlim = 100, verbose = FALSE) {
   # Subset data to relevant, user-specified columns
   data <- data[, c(Y, W, D, Z)]
 
