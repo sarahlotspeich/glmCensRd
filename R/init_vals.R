93ace1d3d99d6735c8fb6d499a59411bf09a5e9e
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2023-02-04 12:20:52 -0500

    Export init_vals()

--- R/init_vals.R
+++ R/init_vals.R
@@ -1,4 +1,14 @@
-init_vals = function(Z, distY, distX) {
+#' Initial values for glmCensRd()
+#'
+#' @param Z (optional) name(s) of additional fully observed covariates. If none, \code{Z = NULL} (the default).
+#' @param distY distribution assumed for \code{Y} given \code{X} and \code{Z}. Default is \code{"normal"}, but other options are \code{"binomial"}, \code{"log-normal"}, \code{"weibull"}, \code{"exponential"}, or \code{"poisson"}.
+#' @param distX distribution assumed for \code{X} given \code{Z}. Default is \code{"normal"}, but other options are \code{"log-normal"}, \code{"gamma"}, \code{"weibull"}, \code{"exponential"}, or \code{"poisson"}.
+#'
+#' @return A vector of parameters
+#'
+#' @export
+#'
+init_vals = function(Z = NULL, distY = "normal", distX = "normal") {
   if (distY %in% c("normal", "log-normal")) {
     params0 = c(rep(0, 2 + length(Z)), 1)
   } else if (distY == "binomial") {
