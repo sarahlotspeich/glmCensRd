e56ad7f99f0ea91ac542cb564f61911c4e7aedba
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2023-03-08 15:53:56 -0500

    Reorder arguments in calc_pYXgivZ() for use with integrate()

--- R/calc_pYXgivZ.R
+++ R/calc_pYXgivZ.R
@@ -1,5 +1,5 @@
 # Calculate probabilities/densities from model of Y,X|Z
-calc_pYXgivZ = function(object, y, x, z) {
+calc_pYXgivZ = function(x, y, z, object) {
   calc_pYgivXZ(object = object, y = y, x = x, z = z) *
     calc_pXgivZ(object = object, x = x, z = z)
 }
