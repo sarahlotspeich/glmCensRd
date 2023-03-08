68655b42d383f51e42bfcdeae7c116a8c60f7a80
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2023-03-08 18:26:48 -0500

    Put back check if (nrow(cens_data)>0) in loglik()

--- R/loglik.R
+++ R/loglik.R
@@ -22,49 +22,43 @@ loglik = function(params, dataObj, returnSum = TRUE, subdivisions){ #params, Y, 
   ####################################################
   # Log-likelihood of uncensored observations ########
   ####################################################
-  ll = cc_loglik(params = params, dataObj = uncens_dataObj)
-  ll = ifelse(test = returnSum, yes = -1, no = 1) * ll # if returnSum, negate ll
-  # if (returnSum) {
-  #   ll = - cc_loglik(params = params,
-  #                    dataObj = uncens_dataObj)
-  # } else {
-  #   ll = cc_loglik(params = params,
-  #                  dataObj = uncens_dataObj,
-  #                  returnSum = returnSum)
-  # }
-  #
+  ll = cc_loglik(params = params,
+                 dataObj = uncens_dataObj)
+  ll = ll * ifelse(test = returnSum,
+                   yes = -1,
+                   no = 1) # if returnSum, negate ll
 
   ####################################################
   # Log-likelihood of censored observations ##########
   ####################################################
-  #if (nrow(cens_dataObj$data) > 0) {
-  integrate_pYXgivZ = function(data_row) {
-    Wi = as.numeric(data_row[cens_dataObj$W])
-    Yi = data_row[cens_dataObj$Y]
-    Zi = data_row[cens_dataObj$Z]
-    return(
-      tryCatch(expr =
-      integrate(f = calc_pYXgivZ,
-                lower = ifelse(test = cens_dataObj$rightCens,
-                               yes = Wi,
-                               no = -Inf),
-                upper = ifelse(test = cens_dataObj$rightCens,
-                               yes = Inf,
-                               no = Wi),
-                subdivisions = subdivisions,
-                y = Yi,
-                z = Zi,
-                object = cens_dataObj)$value,
-      error = function(err) {0})
-    )
+  if (nrow(cens_dataObj$data) > 0) {
+    integrate_pYXgivZ = function(data_row) {
+      Wi = as.numeric(data_row[cens_dataObj$W])
+      Yi = data_row[cens_dataObj$Y]
+      Zi = data_row[cens_dataObj$Z]
+      return(
+        tryCatch(expr =
+        integrate(f = calc_pYXgivZ,
+                  lower = ifelse(test = cens_dataObj$rightCens,
+                                 yes = Wi,
+                                 no = -Inf),
+                  upper = ifelse(test = cens_dataObj$rightCens,
+                                 yes = Inf,
+                                 no = Wi),
+                  subdivisions = subdivisions,
+                  y = Yi,
+                  z = Zi,
+                  object = cens_dataObj)$value,
+        error = function(err) {0})
+      )
+    }
+    int_pYXgivZ_cens = apply(X = cens_dataObj$data,
+                             MARGIN = 1,
+                             FUN = integrate_pYXgivZ)
+    log_int_pYXgivZ_cens = log(int_pYXgivZ_cens)
+    log_int_pYXgivZ_cens[log_int_pYXgivZ_cens == -Inf] = 0
+    ll = ll + sum(log_int_pYXgivZ_cens)
   }
-  int_pYXgivZ_cens = apply(X = cens_dataObj$data,
-                           MARGIN = 1,
-                           FUN = integrate_pYXgivZ)
-  log_int_pYXgivZ_cens = log(int_pYXgivZ_cens)
-  log_int_pYXgivZ_cens[log_int_pYXgivZ_cens == -Inf] = 0
-  ll = ll + sum(log_int_pYXgivZ_cens)
-  #}
 
   # Return (-1) x log-likelihood for use with nlm() --
   return(- ll)
