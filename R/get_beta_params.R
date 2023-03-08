f4a94f7164f675508f1709054759c14f430a61f3
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2023-03-08 11:43:09 -0500

    Create get_beta_params()

---
+++ R/get_beta_params.R
@@ -0,0 +1,34 @@
+# Get parameters for model of Y|X,Z
+get_beta_params = function(object) {
+  UseMethod("get_beta_params")
+}
+
+get_beta_params.normalY = function(object) {
+  lengthZ = length(object$Z)
+  loglikObj$params[1:(3 + lengthZ)]
+}
+
+get_beta_params.lognormalY = function(object) {
+  lengthZ = length(object$Z)
+  loglikObj$params[1:(3 + lengthZ)]
+}
+
+get_beta_params.gammaY = function(object) {
+  lengthZ = length(object$Z)
+  loglikObj$params[1:(3 + lengthZ)]
+}
+
+get_beta_params.weibullY = function(object) {
+  lengthZ = length(object$Z)
+  loglikObj$params[1:(3 + lengthZ)]
+}
+
+get_beta_params.exponentialY = function(object) {
+  lengthZ = length(object$Z)
+  loglikObj$params[1:(2 + lengthZ)]
+}
+
+get_beta_params.poissonY = function(object) {
+  lengthZ = length(object$Z)
+  loglikObj$params[1:(2 + lengthZ)]
+}
