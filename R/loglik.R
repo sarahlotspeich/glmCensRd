b1629fe433902788093d52af9f2ee35465180984
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2023-03-08 16:36:12 -0500

    Comment out tryCatch() in loglik()

--- R/loglik.R
+++ R/loglik.R
@@ -29,13 +29,14 @@ loglik = function(params, dataObj, subdivisions){ #params, Y, X, W, D, Z = NULL,
   ####################################################
   # Log-likelihood of censored observations ##########
   ####################################################
-  if (nrow(cens_dataObj$data) > 0) {
+  #if (nrow(cens_dataObj$data) > 0) {
     integrate_pYXgivZ = function(data_row) {
       Wi = as.numeric(data_row[cens_dataObj$W])
       Yi = data_row[cens_dataObj$Y]
       Zi = data_row[cens_dataObj$Z]
       return(
-        tryCatch(expr = integrate(f = calc_pYXgivZ,
+        #tryCatch(expr =
+        integrate(f = calc_pYXgivZ,
                                   lower = ifelse(test = cens_dataObj$rightCens,
                                                  yes = Wi,
                                                  no = -Inf),
@@ -45,8 +46,8 @@ loglik = function(params, dataObj, subdivisions){ #params, Y, X, W, D, Z = NULL,
                                   subdivisions = subdivisions,
                                   y = Yi,
                                   z = Zi,
-                                  object = cens_dataObj)$value,
-                 error = function(err) {0})
+                                  object = cens_dataObj)$value#,
+                 #error = function(err) {0})
       )
     }
     int_pYXgivZ_cens = apply(X = cens_dataObj$data,
@@ -54,9 +55,10 @@ loglik = function(params, dataObj, subdivisions){ #params, Y, X, W, D, Z = NULL,
                              FUN = integrate_pYXgivZ)
     log_int_pYXgivZ_cens = log(int_pYXgivZ_cens)
     log_int_pYXgivZ_cens[log_int_pYXgivZ_cens == -Inf] = 0
+    print(summary(log_int_pYXgivZ_cens))
     ll = ll + sum(log_int_pYXgivZ_cens)
     print(ll)
-  }
+  #}
 
   # Return (-1) x log-likelihood for use with nlm() --
   return(- ll)
