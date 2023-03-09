d280935c1134a6f31d19897203a98529d0470bc8
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2023-03-09 16:10:52 -0500

    Commit revised calc_deriv_loglik() using class-based functions
    And no longer using calc_indiv_loglik()

---
+++ R/calc_deriv_loglik.R
@@ -0,0 +1,63 @@
+calc_deriv_loglik = function(object, subdivisions) {
+  # Save constants
+  p = length(params)
+  eps = params * (10 ^ (- 4))
+
+  # Create matrix to save derivatives in
+  d_theta = matrix(data = 0,
+                   nrow = nrow(object$data),
+                   ncol = p,
+                   byrow = FALSE)
+
+  # Calculate derivative with respect to each parameter
+  for (j in 1:p) {
+    # Create jth euclidean vector
+    ej = matrix(data = 0,
+                 nrow = p,
+                 ncol = 1)
+    ej[j] = 1
+
+    # Perturb 1: params0 = params - eps
+    params0 = matrix(data = params - eps * ej,
+                     nrow = p,
+                     ncol = 1)
+    l0 = loglik(params = params0,
+                dataObj = object,
+                returnSum = FALSE,
+                subdivisions = subdivisions)
+    # l0 = calc_indiv_loglik(params = params0,
+    #                        Y = Y,
+    #                        X = X,
+    #                        W = W,
+    #                        D = D,
+    #                        Z = Z,
+    #                        subdivisions = subdivisions,
+    #                        distY = distY,
+    #                        distX = distX,
+    #                        data = data)
+
+    # Perturb 2: params1 = params + eps
+    params1 = matrix(data = params + eps * ej,
+                     nrow = p,
+                     ncol = 1)
+    l1 = loglik(params = params1,
+                dataObj = object,
+                returnSum = FALSE,
+                subdivisions = subdivisions)
+    # l1 = calc_indiv_loglik(params = params1,
+    #                        Y = Y,
+    #                        X = X,
+    #                        W = W,
+    #                        D = D,
+    #                        Z = Z,
+    #                        subdivisions = subdivisions,
+    #                        distY = distY,
+    #                        distX = distX,
+    #                        data = data)
+
+    # Estimate deriv = (l(beta + eps) - l(beta - eps)) / (2 * eps)
+    d_theta[, j] = (l1 - l0) / (2 * eps[j])
+  }
+
+  return(d_theta)
+}
