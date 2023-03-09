841804b6c22e6210638477a56d5c2ac73bbe6e5a
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2023-03-09 16:23:53 -0500

    Add default params = NULL to calc_deriv2_loglik()

--- R/calc_deriv2_loglik.R
+++ R/calc_deriv2_loglik.R
@@ -1,19 +1,19 @@
-calc_deriv2_loglik = function(dataObj, subdivisions) {
+calc_deriv2_loglik = function(params = NULL, dataObj, subdivisions) {
   # If no params supplied, take them from dataObj
   if (is.null(params)) {
     params = dataObj$params
   }
-  
+
   # Save constants
   p = length(params)
   eps = params * (10 ^ (- 4))
 
   # Create matrix to save derivatives in
-  d_theta = matrix(data = 0, 
-                   nrow = nrow(data), 
+  d_theta = matrix(data = 0,
+                   nrow = nrow(data),
                    ncol = p ^ 2)
   start_col = 1
-  
+
   # Calculate derivatives with respect to each pair of parameters
   for (j in 1:p) {
     # Create jth euclidean vector
@@ -21,8 +21,8 @@ calc_deriv2_loglik = function(dataObj, subdivisions) {
     ej[j] = 1
 
     # Perturb 1: params0 = params - eps
-    params0 = matrix(data = params - eps * ej, 
-                     nrow = p, 
+    params0 = matrix(data = params - eps * ej,
+                     nrow = p,
                      ncol = 1)
     d_l0 = calc_deriv_loglik(params = params0,
                              dataObj = conv_dataObj,
@@ -30,12 +30,12 @@ calc_deriv2_loglik = function(dataObj, subdivisions) {
       #calc_deriv_loglik(params = params0, Y = Y, X = X, W = W, D = D, Z = Z, subdivisions = subdivisions, distY = distY, distX = distX, data = data)
 
     # Perturb 2: params1 = params + eps
-    params1 = matrix(data = params + eps * ej, 
-                     nrow = p, 
+    params1 = matrix(data = params + eps * ej,
+                     nrow = p,
                      ncol = 1)
     d_l1 = calc_deriv_loglik(params = params1,
                              dataObj = conv_dataObj,
-                             subdivisions = subdivisions) 
+                             subdivisions = subdivisions)
       #calc_deriv_loglik(params = params1, Y = Y, X = X, W = W, D = D, Z = Z, subdivisions = subdivisions, distY = distY, distX = distX, data = data)
 
     # Estimate deriv = (l'(beta + eps) - l'(beta - eps)) / (2 * eps)
