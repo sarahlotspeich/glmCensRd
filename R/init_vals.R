61f62d03dd36d672605914e2dd8f17f74097f5dc
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2023-03-26 10:40:10 -0400

    Replace data[, X] with data[, "X"] in init_vals()

--- R/init_vals.R
+++ R/init_vals.R
@@ -41,11 +41,11 @@ init_valsX = function(object) {
 }
 
 init_valsX.normalX = function(object) {
-  c(rep(0, 1 + length(object$Z)), sd(with(object, data[, X]), na.rm = TRUE))
+  c(rep(0, 1 + length(object$Z)), sd(with(object, data[, "X"]), na.rm = TRUE))
 }
 
 init_valsX.lognormalX = function(object) {
-  c(rep(0, 1 + length(object$Z)), sd(with(object, data[, X]), na.rm = TRUE))
+  c(rep(0, 1 + length(object$Z)), sd(with(object, data[, "X"]), na.rm = TRUE))
 }
 
 init_valsX.gammaX = function(object) {
