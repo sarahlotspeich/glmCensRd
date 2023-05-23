1f62f435b06d9c875240af24be668dc8ad2ee8b1
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2023-05-23 12:31:42 -0400

    Clean up spacing in integrate_loglik.R

--- R/integrate_loglik.R
+++ R/integrate_loglik.R
@@ -3,27 +3,29 @@ integrate_loglik = function(object, w, y, z, subdivisions) {
 }
 
 integrate_loglik.rightCensRd = function(object, w, y, z, subdivisions) {
-  tryCatch(expr =
-             integrate(f = calc_pYXgivZ,
-                       lower = w,
-                       upper = Inf,
-                       subdivisions = subdivisions,
-                       y = y,
-                       z = z,
-                       object = object)$value,
-           error = function(err) {0}
+  tryCatch(
+    expr = integrate(
+      f = calc_pYXgivZ,
+      lower = w,
+      upper = Inf,
+      subdivisions = subdivisions,
+      y = y,
+      z = z,
+      object = object)$value,
+    error = function(err) {0}
   )
 }
 
 integrate_loglik.leftCensRd = function(object, w, y, z, subdivisions) {
-  tryCatch(expr =
-             integrate(f = calc_pYXgivZ,
-                       lower = -Inf,
-                       upper = w,
-                       subdivisions = subdivisions,
-                       y = y,
-                       z = z,
-                       object = object)$value,
-           error = function(err) {0}
-           )
+  tryCatch(
+    expr = integrate(
+      f = calc_pYXgivZ,
+      lower = -Inf,
+      upper = w,
+      subdivisions = subdivisions,
+      y = y,
+      z = z,
+      object = object)$value,
+    error = function(err) {0}
+  )
 }
