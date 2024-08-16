2ea9342772a771d918a253da588b369673b0c19b
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2024-08-16 14:33:06 -0400

    Un-classify get_beta_params (.R)

--- R/get_beta_params.R
+++ R/get_beta_params.R
@@ -1,6 +1,21 @@
 # Get parameters for model of Y|X,Z
 get_beta_params = function(object) {
-  UseMethod("get_beta_params")
+  #UseMethod("get_beta_params")
+  if (object$distY == "normal") {
+    get_beta_params.normalY(object = object)
+  } else if (object$distY == "bernoulli") {
+    get_beta_params.bernoulliY(object = object)
+  } else if (object$distY == "lognormal") {
+    get_beta_params.lognormalY(object = object)
+  } else if (object$distY == "gamma") {
+    get_beta_params.gammaY(object = object)
+  } else if (object$distY == "weibull") {
+    get_beta_params.weibullY(object = object)
+  } else if (object$distY == "exponential") {
+    get_beta_params.exponentialY(object = object)
+  } else if (object$distY == "poisson") {
+    get_beta_params.poissonY(object = object)
+  }
 }
 
 get_beta_params.normalY = function(object) {
